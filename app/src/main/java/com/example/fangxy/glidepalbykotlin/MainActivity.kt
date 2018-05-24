c2bcffc3c4347b060e49bd69a1c35bb95bb81aeb
Fang <296994719@qq.com>
2018-05-24 21:38:55 +0800

    third

--- app/src/main/java/com/example/fangxy/glidepalbykotlin/MainActivity.kt
+++ app/src/main/java/com/example/fangxy/glidepalbykotlin/MainActivity.kt
@@ -1,12 +1,10 @@
-package com.example.fangxy.glidepalbykotlin
-
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.ImageView
-import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
+import com.example.fangxy.glidepalbykotlin.R
 
 class MainActivity : AppCompatActivity() {
 
@@ -25,7 +23,11 @@ class MainActivity : AppCompatActivity() {
 
         val requestedOrientation :RequestOptions = RequestOptions()
                 .error(null)
-        Glide.with(this).load("https://avatar.csdn.net/A/A/8/1_maskedfang.jpg?1527152743").apply(requestedOrientation).into(iv!!)
+//        Glide.with(this).load("https://avatar.csdn.net/A/A/8/1_maskedfang.jpg?1527152743").apply(requestedOrientation).into(iv!!)
 
+       /* GlidepalUtil.getInstance().setOptions(R.drawable.ic_launcher_foreground,R.drawable.ic_launcher_background,false).
+                glideLoad(this,"https://avatar.csdn.net/A/A/8/1_maskedfang.jpg?1527152743",iv!!)*/
     }
+
+
 }

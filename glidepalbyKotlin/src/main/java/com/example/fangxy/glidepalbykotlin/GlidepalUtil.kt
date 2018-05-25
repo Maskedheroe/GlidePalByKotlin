b3a38e06c145c5cb6dee8c1008f3316aafd2e94d
Fang <296994719@qq.com>
2018-05-25 15:39:18 +0800

    add source

--- glidepalbyKotlin/src/main/java/com/example/fangxy/glidepalbykotlin/GlidepalUtil.kt
+++ glidepalbyKotlin/src/main/java/com/example/fangxy/glidepalbykotlin/GlidepalUtil.kt
@@ -6,6 +6,7 @@ import android.widget.ImageView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.request.RequestOptions
+import java.security.CodeSource
 
 /* Glide 加载 简单判空封装 防止异步加载数据时调用Glide 抛出异常
 
@@ -89,6 +90,10 @@ class GlidepalUtil private constructor() {
             Glide.with(context).load(url).apply(requestOptions!!).into(imageView)
 
     }
+    public fun glideLoad(context: Context, url :Int, imageView: ImageView){
+        Glide.with(context).load(url).apply(requestOptions!!).into(imageView)
+
+    }
 
     /*
     * @author
@@ -99,7 +104,11 @@ class GlidepalUtil private constructor() {
     * @FOR THE DOWN
     */
     public fun simpleLoad(context: Context, url :String, imageView: ImageView){
-        Glide.with(context).load(url).apply(requestOptions!!).into(imageView)
+        Glide.with(context).load(url).into(imageView)
+    }
+
+    public fun simpleLoad(context: Context, drawableSource :Int, imageView: ImageView){
+        Glide.with(context).load(drawableSource).into(imageView)
     }
 
 
